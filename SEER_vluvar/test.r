b8fdcff7c2d7860649f6c0f210347a65eb415c98
ggvis <wangzhikun@pku.edu.cn>
2025-01-03 01:52:37 +0800

    try

--- SEER_vluvar/test.r
+++ SEER_vluvar/test.r
@@ -1,9 +1,34 @@
+# 加载tidyverse包
 library(tidyverse)
-data <- read_csv("data.csv")
 
-data |> 
-  janitor::clean_names() |> # 清理列名
-  select(-sex) |> # 删除无用列
-mutate(age_recode_with_1_year_olds = parse_number(age_recode_with_1_year_olds)) |>
-rename(age = age_recode_with_1_year_olds) |>
-  glimpse()
+# 读取数据并清理列名
+data_origin <- read_csv("data.csv") |> janitor::clean_names() 
+
+# 数据清洗:
+# 1. 过滤掉生存月份未知的记录
+# 2. 过滤掉死亡原因未知的记录
+# 3. 转换并过滤肿瘤大小数据
+data <- data_origin |> 
+  filter(survival_months != "Unknown",
+         seer_other_cause_of_death_classification != "Dead (missing/unknown COD)",
+         seer_cause_specific_death_classification != "Dead (missing/unknown COD)") |> 
+  mutate(cs_tumor_size_2004_2015 = parse_number(cs_tumor_size_2004_2015)) |> 
+  filter(cs_tumor_size_2004_2015 > 0 & cs_tumor_size_2004_2015 < 990) 
+
+# 重命名年龄变量
+data <- data |>
+  rename(all_of(
+    c("age" = "age_recode_with_1_year_olds")
+  ))
+
+data |>  select(-sex, 
+                -race_recode_w_b_ai_api, 
+                cs_tumor_size_ext_eval_2004_2015) |> 
+  mutate(age = parse_number(age)) |> 
+  mutate(age = factor(case_when(
+           age <= 64 ~ "<=64",
+           age >= 85 ~ ">=85",
+           TRUE ~ "65-84"
+         ))) |>
+  mutate(race_recode_white_black_other = factor(race_recode_white_black_other)) |> 
+  count(survival_months) |> arrange(desc(survival_months))
--- SEER_vluvar/vluvar.r
+++ SEER_vluvar/vluvar.r
@@ -1,6 +1,33 @@
-# 读取SLM文件
-male_breast_cancer <- readRDS("male_breast_cancer.slm")
+# 加载tidyverse包
+library(tidyverse)
 
-# 检查数据
-head(male_breast_cancer)
-str(male_breast_cancer)
+# 读取数据并清理列名
+data_origin <- read_csv("data.csv") |> janitor::clean_names() 
+
+# 数据清洗:
+# 1. 过滤掉生存月份未知的记录
+# 2. 过滤掉死亡原因未知的记录
+# 3. 转换并过滤肿瘤大小数据
+data <- data_origin |> 
+  filter(survival_months != "Unknown",
+         seer_other_cause_of_death_classification != "Dead (missing/unknown COD)",
+         seer_cause_specific_death_classification != "Dead (missing/unknown COD)") |> 
+  mutate(cs_tumor_size_2004_2015 = parse_number(cs_tumor_size_2004_2015)) |> 
+  filter(cs_tumor_size_2004_2015 > 0 & cs_tumor_size_2004_2015 < 990) 
+
+
+data <- data |>
+  rename(all_of(
+    c("age" = "age_recode_with_1_year_olds") # 年龄
+  ))
+
+data |> 
+  # age 再分组
+  mutate(age = parse_number(age)) |> 
+  mutate(age = factor(case_when(
+    age <= 64 ~ "<=64",
+    age >= 85 ~ ">=85",
+    TRUE ~ "65-84"
+  ))) |>
+  count(derived_ajcc_stage_group_6th_ed_2004_2015) |> 
+  mutate(n_prob = n / sum(n))
